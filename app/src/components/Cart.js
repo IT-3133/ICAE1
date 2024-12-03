e08f898290acecf801351afae3ae664d0f7de158
Thilankavishka <thilankavishka25@gmail.com>
2024-12-03 18:30:59 +0530

    Create Cart Part

--- app/src/components/Cart.js
+++ app/src/components/Cart.js
@@ -1,4 +1,9 @@
 export default function Cart({ cartItems }) {
+  const grandTotal = cartItems.reduce(
+    (acc, item) => acc + item.price * item.qty,
+    0
+  );
+
   return (
     <div className="table-container">
       <h4 className="card-title">Cart</h4>
@@ -10,11 +15,19 @@ export default function Cart({ cartItems }) {
             <th>Price</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {cartItems.map((item) => (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.qty}</td>
+              <td>{item.price * item.qty}</td>
+            </tr>
+          ))}
+        </tbody>
         <tfoot>
           <tr>
             <td>Grand Total:</td>
-            <td colSpan="2">{}</td>
+            <td colSpan="2">{grandTotal}</td>
           </tr>
         </tfoot>
       </table>
